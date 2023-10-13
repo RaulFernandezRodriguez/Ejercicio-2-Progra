9bbcfbb78918bad76d9de94b5034458da5932cf0
Raul <rfernr08@estudiantes.unileon.es>
2023-10-13 11:25:55 +0200

    pequeños cambios

--- src/MatrizSimetrica.java
+++ src/MatrizSimetrica.java
@@ -2,17 +2,22 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class App {
+public class MatrizSimetrica {
     public static void main(String[] args) throws Exception {
-        Scanner stdinput = new Scanner();
-        int size = stdinput.next;
+        Scanner stdinput = new Scanner(System.in);
+        int size = stdinput.nextInt();
         assert size > 0;
-        ArrayList matriz = new ArrayList<int>(size);
-        rellenarmatriz();
+        int[][] matriz = new int[size][size];
+        rellenarMatriz(0, 0);
         comprobarsimetria();
         
         System.out.println("Entrada errónea");
         System.out.println("Es simétrica la raiz cuadrada entera de la matriz de entrada");
         System.out.println("No es simétrica la raiz cuadrada entera de la matriz de entrada");
+    }
+
+    public static void rellenarMatriz(int x, int j){
+        int input = stdinput.nextInt();
+
     }
 }
