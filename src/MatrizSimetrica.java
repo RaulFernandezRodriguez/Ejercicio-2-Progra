e2431e2e4223bc2863ff799fd555a1fae5b5dc58
Raul <rfernr08@estudiantes.unileon.es>
2023-10-15 19:17:19 +0200

    funcional creo

--- src/MatrizSimetrica.java
+++ src/MatrizSimetrica.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 import java.math.BigInteger;
-
-
 public class MatrizSimetrica {
     static final Scanner stdinput = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
@@ -37,7 +35,7 @@ public class MatrizSimetrica {
     
     public static void rellenarFila(int[][] matriz, int x, int y, String[] fila){
         if(y < matriz[x].length){
-            matriz[x][y] = Integer.parseInt(fila[y]);
+            matriz[x][y] = (int) Math.sqrt(Integer.parseInt(fila[y]));
             rellenarFila(matriz, x, y+1, fila);
         }
     }
@@ -45,13 +43,7 @@ public class MatrizSimetrica {
     public static void transponer(int[][] matriz,int[][] matrizT, int x, int y){
         if (x < matriz.length) {
             if (y < matriz[x].length) {
-                // int input = stdinput.nextInt();
-                // assert input >= 0;
-                // matriz[x][y] = (int) Math.sqrt(input);
                 matrizT[y][x] = matriz[x][y];
-                // if(matriz[x][y] != matrizT[x][y]){
-                //     simetric = false;
-                // }
                 transponer(matriz, matrizT, x, y + 1);
             } else {
                 transponer(matriz, matrizT,  x + 1, 0);
@@ -70,15 +62,4 @@ public class MatrizSimetrica {
             }else return false;
         } 
     }
-    //     if(y == matriz[y].length){
-    //         return true;
-    //     }else if(x == matriz[x].length){
-    //         return comprobarSimetria(matriz, matrizT, 0, y+1);
-    //     }else{
-    //         if(matriz[x][y] != matrizT[x][y]){
-    //             return false;
-    //         }
-    //         return comprobarSimetria(matriz, matrizT, x+1, y);
-    //     } 
-    //}
 }
