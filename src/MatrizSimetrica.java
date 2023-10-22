7340415ce6b26c706fc71df5f26a9b0073965ff5
Raul <rfernr08@estudiantes.unileon.es>
2023-10-22 17:42:30 +0200

    reducion

--- src/MatrizSimetrica.java
+++ src/MatrizSimetrica.java
@@ -9,9 +9,8 @@ public class MatrizSimetrica {
             if(size > 0){
                 int[][] matriz = new int[size][size];
                 int[][] matrizT = new int[size][size];
-                rellenarMatriz(matriz, 0, 0);
+                rellenarMatriz(matriz,matrizT, 0, 0);
                 stdinput.close();
-                transponer(matriz, matrizT, 0, 0);
                 boolean simetric = comprobarSimetria(matriz, matrizT, 0, 0);
                 if(simetric){
                     System.out.println("Es simétrica la raíz cuadrada entera de la matriz de entrada.");
@@ -19,42 +18,31 @@ public class MatrizSimetrica {
                     System.out.println("No es simétrica la raíz cuadrada entera de la matriz de entrada.");
                 }
             }else{            
-                System.out.println("Entrada errónea");
+                System.out.println("Entrada errónea.");
             }
         } else {
-            System.out.println("Entrada errónea");
+            System.out.println("Entrada errónea.");
         }
     } 
 
-    public static void rellenarMatriz(int[][] matriz, int x, int y){
+    public static void rellenarMatriz(int[][] matriz, int[][] matrizT, int x, int y){
         if(x < matriz.length){
             String filaInput = stdinput.nextLine();
             if (!filaInput.trim().isEmpty()) {
                 String[] fila = filaInput.split(" ");
                 assert fila.length == matriz[y].length;
-                rellenarFila(matriz, x, 0, fila);
-                rellenarMatriz(matriz, x+1, 0);
+                rellenarFila(matriz, matrizT, x, 0, fila);
+                rellenarMatriz(matriz, matrizT, x+1, 0);
             }
         }
     } 
     
-    public static void rellenarFila(int[][] matriz, int x, int y, String[] fila){
+    public static void rellenarFila(int[][] matriz, int[][] matrizT, int x, int y, String[] fila){
         if(y < matriz[x].length){
-            matriz[x][y] = (int) Math.sqrt(Integer.parseInt(fila[y]));
-            rellenarFila(matriz, x, y+1, fila);
+            matrizT[y][x] = matriz[x][y] = (int) Math.sqrt(Integer.parseInt(fila[y]));
+            rellenarFila(matriz, matrizT, x, y+1, fila);
         }
     }
-
-    public static void transponer(int[][] matriz,int[][] matrizT, int x, int y){
-        if (x < matriz.length) {
-            if (y < matriz[x].length) {
-                matrizT[y][x] = matriz[x][y];
-                transponer(matriz, matrizT, x, y + 1);
-            } else {
-                transponer(matriz, matrizT,  x + 1, 0);
-            }
-        }  
-    } 
 
     public static boolean comprobarSimetria(int[][] matriz,int[][] matrizT, int x, int y){
         if (x == matriz.length) {
